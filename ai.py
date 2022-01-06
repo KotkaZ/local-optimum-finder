e9360b80b15b0a4bdf9c71de7c3b806a4f7ca95e
tvaino <tiitvaino25@gmail.com>
2022-01-06 20:51:57 +0200

    Got worikig

--- ai.py
+++ ai.py
@@ -2,7 +2,9 @@ from image_processing import transform_images
 from ai_trainer_predicter import train, load_from_dir, predict
 import sys
 import cv2 as cv
+import os
 import numpy as np
+
 BUILD_DIR__USER = "./build/user/"
 IMG_PATH = sys.argv[1]
 DO_TRAIN = sys.argv[2] == "True"
@@ -10,6 +12,9 @@ print(DO_TRAIN)
 
 if DO_TRAIN:
     train()
+
+if not os.path.isdir(BUILD_DIR__USER):
+    os.makedirs(BUILD_DIR__USER)
 
 path = IMG_PATH
 
@@ -26,13 +31,14 @@ cv.imwrite(BUILD_DIR__USER+"user.png", crop_img)
 imgs_norm = []
 imgs = []
 
-img = crop_img
+img = cv.imread(BUILD_DIR__USER+"user.png")
 
 # Normaliseerime pildi
 norm_img = np.zeros(shape = (130,210,1))
 for ri, row in enumerate(img):
     for pi, px in enumerate(row):
         norm_img[ri][pi][0] = px[0]/255
+
 imgs_norm.append(norm_img)
 imgs.append(img)
 imgs_norm = np.array(imgs_norm)
