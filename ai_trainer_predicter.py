06602f20f3d7c91040ef2f90d477b1334ac05186
tvaino <tiitvaino25@gmail.com>
2022-01-06 16:21:49 +0200

    Got model predicting, put not right

--- ai_trainer_predicter.py
+++ ai_trainer_predicter.py
@@ -2,7 +2,9 @@ import cv2 as cv
 import numpy as np
 import os
 
-from keras.layers import MaxPooling2D, BatchNormalization, Dropout
+from sklearn.model_selection import train_test_split
+
+from keras.layers import MaxPooling2D, BatchNormalization, Dropout, Reshape
 from keras.models import Model
 from keras.layers import Input, Conv2D, Activation, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
@@ -31,13 +33,14 @@ def load_from_dir(path:str)-> list:
             print("There is no {}".format(path))
             continue
         # Normaliseerime pildi
-        norm_img = np.zeros(shape = (130,210,1))
+        #norm_img = np.zeros(shape = (130,210,1))
+        norm_img = [[[0] for b in range (210)] for a in range(130)]
         for ri, row in enumerate(img):
             for pi, px in enumerate(row):
                 norm_img[ri][pi][0] = px[0]/255
         data.append(norm_img)
 
-    return np.array(data)
+    return data
 
 def get_train_data():
     if not os.path.isfile(BUILD_DIR+"coordinates.txt"):
@@ -54,35 +57,55 @@ def get_train_data():
             for x in splitted_line:
                 locs.append(tuple(int(y) for y in x.split(",")))
 
-            data = [0 for el in range(210*130)]
+            data = [[[0] for b in range (210)] for a in range(130)]
             for loc in locs:
-                data[loc[0]*loc[1]]= 1
+                for i in range(6):
+                    for j in range(6):
+                        x = 3-i
+                        y = 3-j
+                        r = x**2 + y**2
+                        val = 0
+                        if(r <= 5):
+                            val = 0.99
+                        elif(r <= 8):
+                            val= 0.85
+                        elif(r <= 9):
+                            val = 0.75
+                        else:
+                            val = 0.60
+                        if not (loc[1]+x < 0 or loc[1]+x >= 130 or loc[0]+y < 0 or loc[0]+x >=210):
+                            data[loc[1]+x][loc[0]+y][0] = val
+                data[loc[1]][loc[0]][0]= 0.999
             coordinates.append(data)
 
-    return (unmarked_imgs, coordinates)
+    return (np.array(unmarked_imgs), np.array(coordinates))
 
 def train_model(X_train, y_train):
     input_shape = X_train[0].shape
     x = Input(shape=input_shape)
-    c1 = Conv2D(input_shape[0], (3, 3), strides=1, padding="same")(x)
+    c1 = Conv2D(64, (3, 3), strides=1, padding="same")(x)
+    c2 = Conv2D(64, (3, 3), strides=1, padding="same")(c1)
+    c3 = Conv2D(4, (3, 3), strides=1, padding="same")(c2)
+    p4 = MaxPooling2D(pool_size=(2,2))(c3)
+    f5 = Flatten()(p4)
+    r6 = Reshape((130,210,1))(f5)
+    """
     b1 = BatchNormalization()(c1)
     a1 = Activation('relu')(b1)
-    c2 = Conv2D(input_shape[0], (3, 3), strides=1, padding="valid")(a1)
     b2 = BatchNormalization()(c2)
     a2 = Activation('relu')(b2)
-    p2 = MaxPooling2D(pool_size=(4,4))(a2)
     d2 = Dropout(0.25)(p2)
     f2 = Flatten()(d2)
-    #h3 = Dense(input_shape[0] * input_shape[1],activation='softmax')(f2)
+    #h3 = Dense(input_shape[0] * input_shape[1],activation='softmax')(f2) """
     """ b3 = BatchNormalization()(h3)
     a3 = Activation('relu')(b3)
     d3 = Dropout(0.25)(a3)
     z = Dense(input_shape[0] + input_shape[1],activation='softmax')(d3) """
 
-    model = Model(inputs=x, outputs=f2)
+    model = Model(inputs=x, outputs=r6)
     model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
     model.summary()
-    history = model.fit(X_train, y_train, batch=256, epochs=1)
+    history = model.fit(X_train, y_train, batch_size=256, epochs=1,verbose = True)
 
     return model, history
 
@@ -90,14 +113,27 @@ def train_model(X_train, y_train):
 def save_model(model):
     model = None
 
-def train():
-    X_train, y_train = get_train_data()
-    model, history = train_model(X_train, y_train)
-    return model, history
 
 
-model, history = train()
-model, history
+
+X_train, y_train = get_train_data()
+X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2, random_state=12)
+model, history = train_model(X_train, y_train)
+
+y_pred = model.predict(X_test)
+test_img = np.zeros((130,210,3))
+for ri, row in enumerate(y_pred[0]):
+    for pi, px in enumerate(row):
+        val = int(255 * px[0])
+        test_img[ri][pi][0] = val
+        test_img[ri][pi][1] = val
+        test_img[ri][pi][2] = val
+cv.imshow("test", test_img)
+cv.waitKey(0)
+# closing all open windows
+cv.destroyAllWindows()
+
+
 #############################
 # Predicter
 #############################
@@ -108,7 +144,7 @@ def model_load():
     return model
 
 def model_predict(model):
-    locs = []
+
     return []
 
 # Adds red dots to the locations, and shows the final answer
